417fd921d3ccd32030df025dce2eba9e36bf3fd3
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2025-08-22 22:11:07 -0400

    Time: 352 ms (65.84%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,13 +1,8 @@
-# Write your MySQL query statement below
-#query <3 is poor query
-with cte as(
-select query_name, rating/position as ratio, case when rating < 3 
-then 1 else 0 end as poor_query
-from Queries
-)
-select query_name,Round(avg(ratio),2) as quality
-,Round(sum(poor_query)/count(query_name) * 100,2) as  poor_query_percentage
-from
-cte
-group by query_name
+select query_name,round(avg(rating/position),2) as quality,
+round(avg(CASE WHEN RATING <3 THEN 1 ELSE 0 END)*100,2)AS
+poor_query_percentage
+from 
+Queries
+GROUP BY query_name
+
 
