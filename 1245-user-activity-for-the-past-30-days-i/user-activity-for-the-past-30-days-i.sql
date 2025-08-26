e276fdd91e02e3ca00082b6c915ca508eaee4db3
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2025-08-26 00:55:50 -0400

    Time: 554 ms (22.38%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -3,4 +3,5 @@ SELECT
   COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE DATEDIFF('2019-07-27', activity_date) between 0 and 29
-GROUP BY activity_date;
+GROUP BY activity_date
+having count(activity_type) >= 1
