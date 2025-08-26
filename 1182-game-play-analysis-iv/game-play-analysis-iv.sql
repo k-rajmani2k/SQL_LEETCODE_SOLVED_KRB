4da06433fefcdcbc5e3cc5f9781ed67b74bb487a
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2025-08-26 00:23:30 -0400

    Time: 527 ms (81.95%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,6 +1,5 @@
-select ROUND(sum(player_logun)/count(distinct player_id),2) as fraction
-from
-(select 
-player_id,datediff(event_date,min(event_date) over(partition by player_id)) = 1 as player_logun
+select round(sum(loginv)/count(distinct player_id),2)  as fraction from
+(
+select player_id,datediff(event_date,min(event_date) over(partition by player_id)) = 1  as loginv
 from Activity
-) AS NN
+) as j    
